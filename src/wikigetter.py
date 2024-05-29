a885aaf19b79657c87fd9fcf32feafae05c53c16
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-05-29 18:02:06 +0200

    Added skeleton

---
+++ src/wikigetter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Wikigetter A bare-minimal summary getter for wikipedia.
+"""
+
+__author__ = "Alessandro Paganelli"
+__version__ = "0.0.1"
+__license__ = "MIT"
+
+import wikipedia
+
+def main():
+    """Main entry point"""
+    pass
+
+if __name__ == "__main__":
+    """Entry point from the commandline"""
+    main()
+
