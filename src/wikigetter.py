a106f19abe10bc33bb1f70e4556e1d9a341bb91f
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-05-29 18:21:46 +0200

    First version

--- src/wikigetter.py
+++ src/wikigetter.py
@@ -10,14 +10,36 @@ __license__ = "MIT"
 import wikipedia
 import argparse
 
+def get_summary(topic: str) -> str:
+    summary = ""
+    results = wikipedia.search(topic)
+
+    if results:
+        summary = wikipedia.summary(results[0])
+
+    return summary
+
 def main():
     """Main entry point"""
     parser = argparse.ArgumentParser(description='Gets a summary from wikipedia.')
     parser.add_argument('topic', metavar='t', type=str, help='The topic to search on wikipedia.')
+    parser.add_argument('outfile', metavar='o', type=str, help='The output file to dump the summary to')
     args = parser.parse_args()
-    print(args.topic)
 
-if __name__ == "__main__":
+    wikipedia.set_lang('it')
+
+    topic = args.topic
+    print('Getting a summary for: {t}'.format(t=topic))
+    summary = get_summary(topic)
+
+    outfile = args.outfile
+    print('Dumping summary to file: {f}'.format(f=outfile))
+    with open(outfile, 'w') as f:
+        f.write(summary)
+
+    print('Done!')
+
+if __name__ == '__main__':
     """Entry point from the commandline"""
     main()
 
